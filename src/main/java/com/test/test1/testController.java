078793ccf46b58adf40b4663bbe8ef370c10dbbe
havana1016 <hasangsu82@gmail.com>
2022-05-16 16:39:32 +0900

    인텔리제이에서 프로젝트 새로 만들어 세팅하기 연습

--- src/main/java/com/test/test1/testController.java
+++ src/main/java/com/test/test1/testController.java
@@ -1,4 +1,13 @@
 package com.test.test1;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class testController {
+    @GetMapping("/")
+    String test(){
+        return ("test");
+    }
 }
